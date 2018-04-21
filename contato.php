fefc789b4f0344db1f63e5fe591d644934aec9b7
Renata Felix <naty.bmth1@gmail.com>
2018-04-21 18:46:56 -0300

    Update contato.php

--- contato.php
+++ contato.php
@@ -1,34 +1,33 @@
 <?php
 
-// Destinat·rio
 $para = "naty.bmth1@gmail.com";
 
-// Assunto do e-mail
+
 $assunto = "Contato atraves do Portifolio :)";
 
-// Campos do formul·rio de contato
+
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
 $mensagem = $_POST['mensagem'];
 
-// Monta o corpo da mensagem com os campos
+
 $corpo = "<html><body>";
 $corpo .= "Nome: $nome ";
 $corpo .= "Email: $email Assunto: $assunto Mensagem: $mensagem";
 $corpo .= "</body></html>";
 
-// CabeÁalho do e-mail
+
 $email_headers = implode("\n", array("From: $nome", "Reply-To: $email", "Subject: $assunto", "Return-Path: $email", "MIME-Version: 1.0", "X-Priority: 3", "Content-Type: text/html; charset=UTF-8"));
 
-//Verifica se os campos est„o preenchidos para enviar ent„o o email
+//Verifica se os campos est√£o preenchidos para enviar ent√£o o email
 if (!empty($nome) && !empty($email) && !empty($mensagem)) {
     mail($para, $assunto, $corpo, $email_headers);
     $msg = "Sua mensagem foi enviada com sucesso.";
-    echo "<script>alert('$msg');window.location.assign('http://www.seusite.com.br/contato');</script>";
+    echo "<script>alert('$msg');window.location.assign('https://natyr.github.io/');</script>";
 } else {
     $msg = "Erro ao enviar a mensagem.";
-    echo "<script>alert('$msg');window.location.assign('http://www.seusite.com.br/contato');</script>";
+    echo "<script>alert('$msg');window.location.assign('https://natyr.github.io/');</script>";
 }
 
 ?>
